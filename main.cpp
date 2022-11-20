cfcf4cfccb1906201d946c78f905563920f7b56f
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-19 20:28:17 -0500

    Project Finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <array>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::array<std::string, 5> drinks = {"Coke", "Water", "Sprite", "Pepsi", "Apple Juice"};
+    for(int i = 1; i < drinks.size() + 1; i++ ){
+        std::cout << i << ". " << drinks[i - 1] << std::endl;
+    }
+
+    int choice;
+    std::cout << "Please pick a number: ";
+    std::cin >> choice;
+    if(choice > drinks.size() || choice < 1){
+        std::cout << "Error, Invalid Choice" << std::endl;
+    }else{
+        std::cout << "Enjoy Your " << drinks[choice - 1] << std::endl;
+    }
     return 0;
 }
